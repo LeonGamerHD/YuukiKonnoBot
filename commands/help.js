67a0c404afb10012ba0463e286de7103c11e77dc
Leon Henning <53613583+LeonGamerHD@users.noreply.github.com>
2020-08-08 16:39:10 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,9 @@ module.exports = {
         const Discord = require('discord.js');
         const embed = new Discord.MessageEmbed()
         .setTitle(name="**Hilfe**")
-        .addField(name="!clear", value="Team Mitgliedern vorbehalten", inline=true)
-        .addField(name="!help", value="Damit kannst du diese Hilfe öffnen", inline=false)
+        .addField(name="Prefix", value="Y/<command>", inline=true)
+        .addField(name="clear", value="Team Mitgliedern vorbehalten", inline=true)
+        .addField(name="help", value="Damit kannst du diese Hilfe öffnen", inline=true)
         .addField(name="#bot-befehle", value="Hier kannst du mir anweisungen geben!", inline=true)
         .addField(name="#rollen-auswahl", value="Hier kannst du auswählen worüber du informiert werden möchtest", inline=true)
         message.channel.send(embed);
